380b07ba36cbb76b017fa91820ebe5128f765467
John Koh <jsk2210@columbia.edu>
2015-02-14 12:36:00 -0500

    fetcher.py: Break out lock responsibility to separate class

--- python/crawler/fetcher.py
+++ python/crawler/fetcher.py
@@ -10,27 +10,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class ChromePackageFetcher:
-
-	def reset_url_params(self):
-		self.url_params = collections.OrderedDict([
-			('response', 'redirect'),
-			('x', None), # Do not touch
-			('uc', None),
-			('id', None),
-			('lang', None),
-			('prod', 'chrome')
-		])
-
-	def __init__(self, url, db, alphabet=AlphabetType.en_US):
+class ApplicationIdLocker:
+	def __init__(self, db, alphabet=AlphabetType.en_US):
 		self.alphabet = alphabet
 		self.db = db
-		self.fetch_point = url
-		logger.info('Fetch point: %s' % self.fetch_point)
-		self.reset_url_params()
-		self.parser = None
+		self.app_id = None
 
-	def lock_app_id(self, key, field, value):
+	def __lock_app_id(self, key, field, value):
 		"""Get a simple lock on a specific app_id in Redis."""
 		lock_name = ':'.join((self.alphabet.lock_prefix(), field))
 
@@ -56,31 +42,51 @@ class ChromePackageFetcher:
 		"""Checks if the app_id timestamp is 0 (never fetched) or old."""
 		return not value
 
-	def get_unused_app_id(self):
+	def set_lock_get_id(self):
 		"""Get an app_id that isn't currently locked for fetching."""
-		app_id = None
-		app_ts = 0
 		key = self.alphabet.name
 
 		for field, value in self.db.hscan_iter(key, count=20):
 			# Check if value is stale
-			if not self.stale(value) and self.lock_app_id(key, field, value):
+			if not self.stale(value) and self.__lock_app_id(key, field, value):
 				logger.info('Got app_id lock: %s', field)
+				self.app_id = field
 				return field
 
 			logger.info('App_id %s was locked. Trying next...' % field)
 
 		logger.warn('Could not find any unlocked and/or stale app_id!')
 
-	def release_app_id_lock(self, app_id):
+	def unlock(self):
 		"""Release the lock on an app_id."""
-		if not app_id:
+		if not self.app_id:
 			return
 
-		lock_name = ':'.join((self.alphabet.lock_prefix(), app_id))
+		lock_name = ':'.join((self.alphabet.lock_prefix(), self.app_id))
 		result = self.db.delete(lock_name)
 		logger.info('Attempted release of lock: %s (result: %s)' % (lock_name, result))
 		return result
+
+
+class ChromePackageFetcher:
+
+	def reset_url_params(self):
+		self.url_params = collections.OrderedDict([
+			('response', 'redirect'),
+			('x', None), # Do not touch
+			('uc', None),
+			('id', None),
+			('lang', None),
+			('prod', 'chrome')
+		])
+
+	def __init__(self, url, db, alphabet=AlphabetType.en_US):
+		self.alphabet = alphabet
+		self.db = db
+		self.fetch_point = url
+		logger.info('Fetch point: %s' % self.fetch_point)
+		self.reset_url_params()
+		self.app_id_locker = ApplicationIdLocker(db=db, alphabet=self.alphabet)
 
 	def fetch_app(self, app_id):
 		import time
@@ -92,11 +98,11 @@ class ChromePackageFetcher:
 		try:
 			# Get an app_id, add to en_US_processing_set (with TTL)
 			# if not already in the set, else get another app_id
-			app_id = self.get_unused_app_id()
+			app_id = self.app_id_locker.set_lock_get_id()
 
 			# Fetch data about that app
 			self.fetch_app(app_id)
 
 			# Release lock
 		finally:
-			self.release_app_id_lock(app_id)
+			self.app_id_locker.unlock()
